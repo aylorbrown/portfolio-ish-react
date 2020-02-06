f67840bc24372ff14a124156d04c349b3514cb98
Aylor Brown <aylorbrown@gmail.com>
2020-02-06 13:51:05 -0500

    adds router to nav

--- src/App.js
+++ src/App.js
@@ -5,25 +5,78 @@ import Footer from './Footer';
 import MainContent from './MainContent';
 import Nav from './Nav';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 const content= `Aylor is a talented web dev. She would be a great addition
 to your team!`;
 const headerContent = `Hire Aylor Brown!`;
 const linkNames = [
-  'Home',
-  'Portfolio',
-  'Contact'
+  {
+    text: 'Home', 
+    path: '/'
+  }, 
+  {
+    text: 'Blog', 
+    path: '/blog'
+
+  }
 ]
 
+const footerLinks = [
+  {
+    href: "mailto:me@me.com", 
+    text: "Email Aylor!"
+  },
+  {
+    href: "tel:40440444040", 
+    text: "Call Aylor!"
+  }
+]
 
 export default function App() {
   return (
-    <div className="container-fluid">
+    <Router>
+      {/* <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+      </ul> */}
+
+      <Nav 
+        links={linkNames}
+      />
+
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/blog">
+          <Blog />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h1>You are Home</h1>;
+}
+
+function Blog() {
+  return <h1>You are on the blog!</h1>
+}
+
+{/* <div className="container-fluid">
       <Header title={headerContent}/>
       <Nav links={linkNames}/>
       <MainContent content={content}/>
       <Footer />
-    </div>
-  );
-}
-
-// export default App;
+    </div> */}
--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // declares that the component is a kind of react component so 
 // let me inherit all that good stuff
@@ -14,25 +15,24 @@ export default class Nav extends React.Component {
     render() {
         return (
             // if/else with a return
-                this.state.isOpen ? 
-                <ul onClick={this._toggleMenu}>
-                    {/* {
-                    links.map((item, index) => {
-                        return<li>{item}</li>
-                    })
-                    } */}
-    
-                    <li>{this.props.links[0]}</li>
-                    <li>{this.props.links[1]}</li>
-                    <li>{this.props.links[2]}</li>
-    
-                </ul>
+            this.state.isOpen ? 
+            <div>
+                <a onClick={this._toggleMenu}>🥞</a>
+                <ul>
+                        {
+                            this.props.links.map((item, index) => {
+                                return <li key={index}>{item}</li>
+                            })
+                        }
+                    </ul>
+            </div>
                 :
                 <a onClick={this._toggleMenu}>🍔</a>
         );
     }
 
     // helper method/function 
+    // arrow function variable never ever lose the value of 'this' keyword
     _toggleMenu = () => {
         // you can't manipulate state directly! react won't know to redraw 
         this.setState({
